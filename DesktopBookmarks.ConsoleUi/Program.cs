82cf4d833bce04dfd9a02644365c8033a4d4aa42
Vitalii Martyniuk <vitaliy.martinyk@gmail.com>
2020-11-07 00:53:16 +0200

    Init project

---
+++ DesktopBookmarks.ConsoleUi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesktopBookmarks.ConsoleUi
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
